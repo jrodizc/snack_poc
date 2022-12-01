deda4b827fcf173e564d263aae18f66599601d3d
Rodiz, Joseph <joseph@whisper.sh>
2022-12-01 10:13:57 -0600

    Update to 1.2.1

--- snacklib/presentation/src/main/java/com/jrodiz/snacklib/data/GreetingRemoteRepository.kt
+++ snacklib/presentation/src/main/java/com/jrodiz/snacklib/data/GreetingRemoteRepository.kt
@@ -5,6 +5,6 @@ import javax.inject.Inject
 
 class GreetingRemoteRepository @Inject constructor() : GreetingRepository {
     override fun getGreeting(): String {
-        return "Greeting from new layer"
+        return "Greeting from new layer :=)"
     }
 }
